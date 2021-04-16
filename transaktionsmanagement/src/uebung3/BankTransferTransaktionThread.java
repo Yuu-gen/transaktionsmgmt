2b2102b811ed2986722ce0f598e05865ff9482fa
Yuu-gen <zel.da.hero.yr+github@gmail.com>
2021-04-16 16:58:51 +0200

    Aufgabe 3 irgendwie gelöst? Jedenfalls sorgen wir für mehr Fehler!

---
+++ transaktionsmanagement/src/uebung3/BankTransferTransaktionThread.java
@@ -0,0 +1,142 @@
+package uebung3;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+
+
+
+public class BankTransferTransaktionThread extends Thread {
+
+	private static final String selectBetrag = "SELECT betrag FROM konten WHERE kunde = ?;";
+	private static final String updateBetrag = "UPDATE konten SET betrag = ? WHERE kunde = ?;";
+	
+	private final String connectionString;
+	private final String sourceAccount;
+	private final String targetAccount;
+	private final BigDecimal amount;
+	private Integer transfercount;
+	private Integer commitcount;
+	private Integer killcount;
+	private Integer rollbackcount;
+	private Connection conn;
+	
+	public BankTransferTransaktionThread(Connection conn, String connString, String source, String target, BigDecimal amount){
+		this.conn = conn;
+		this.connectionString = connString;
+		this.sourceAccount = source;
+		this.targetAccount = target;
+		this.amount = amount;
+		this.transfercount = 0;
+		this.commitcount = 0;
+		this.killcount = 0;
+		this.rollbackcount = 0;
+	}
+	
+	@Override
+	public void run() {
+		try(Connection conn = this.conn){
+		//try(Connection conn = DriverManager.getConnection(this.connectionString)){
+			for (int i = 0; i < 1000; i++) {
+			bankTransfer(conn, this.sourceAccount, 
+					this.targetAccount, this.amount);
+			}
+			System.out.printf("%s hat %d Überweisungen ausgeführt.%n", this.getName(),this.transfercount/2);
+			System.out.printf("%s hat %d Commits ausgeführt.%n", this.getName(),this.commitcount);
+			System.out.printf("%s hat %d Rollbacks ausgeführt.%n", this.getName(),this.rollbackcount);
+		}catch (SQLException e) {
+			// TODO: handle exception
+		}
+	}
+	
+	
+	
+	protected void bankTransfer(Connection conn, String von, String nach, BigDecimal betrag) {
+		final BigDecimal ueberweisungsbetrag = BigDecimal.valueOf(100, 0);
+		
+		try {
+			conn.setAutoCommit(false);
+			this.killcount++;
+			
+			updateKontoBetrag(conn, von, ueberweisungsbetrag.negate());
+			updateKontoBetrag(conn, nach, ueberweisungsbetrag);
+			if(this.killcount % 2 == 0) {
+			conn.commit();
+			this.commitcount++;
+			}else
+			{
+				throw new SQLException();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			//System.err.println(e.getErrorCode());
+			try {
+				
+				conn.rollback();
+				this.rollbackcount++;
+				//System.out.println("ROLLBACK");
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				//System.err.println(e1.getErrorCode());
+				System.out.println("Couldn't rollback. You are on your own now.");
+			}
+		}
+		
+		
+		
+	}
+
+
+	/**
+	 * Aktualisieren des Kontostandes für einen Kunden mit bestimmtem Namen
+	 * @param conn Eine aktive JDBC-Datenbankverbindung
+	 * @param konto Name des Kunden // FIXME: better name!
+	 * @param aenderung wird auf den aktuellen Kontostand addiert; 
+	 *        positive Werte erhöhen den Kontostand, negative verringern den Kontostand
+	 * @throws SQLException 
+	 */
+	private void updateKontoBetrag(Connection conn, String konto, final BigDecimal aenderung) throws SQLException {
+		try (final PreparedStatement stmt = conn.prepareStatement(selectBetrag)) {
+			stmt.setString(1, konto);
+			final ResultSet rs = stmt.executeQuery();
+			if (rs.next()) {
+				BigDecimal kontostandVon = rs.getBigDecimal("betrag");
+				//System.out.printf("Der Kontostand von %s beträgt %s.%n", konto, kontostandVon);
+				kontostandVon = kontostandVon.add(aenderung);
+				//System.out.printf("Der Kontostand von %s soll geändert werden auf %s.%n", konto, kontostandVon);
+				
+				try (final PreparedStatement us = conn.prepareStatement(updateBetrag)) {
+					us.setBigDecimal(1, kontostandVon);
+					us.setString(2, konto);
+					int count = us.executeUpdate();
+					if (count == 1) {
+						//System.out.println("Kontostand geändert");
+					}
+				}	
+				
+			} else {
+				//System.err.printf("Kunde %s nicht gefunden%n", konto);
+			}
+			final ResultSet afterRS = stmt.executeQuery();
+			if (afterRS.next()) {
+				BigDecimal kontostandVon = afterRS.getBigDecimal("betrag");
+				//System.out.printf("Der Kontostand von %s beträgt nun %s.%n", konto, kontostandVon);
+			}else {
+				//System.err.printf("Kunde %s nicht gefunden%n", konto);
+			}
+			this.transfercount++;
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			//e.printStackTrace();
+			throw(e);
+		}
+	}	
+
+	
+}
---
+++ transaktionsmanagement/src/uebung3/ConcurrencyTransactionTest.java
@@ -0,0 +1,120 @@
+package uebung3;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import uebung2.BanktransferThread;
+
+public class ConcurrencyTransactionTest {
+
+	private static final String deleteKontenSQL = "DELETE FROM konten;";
+	private static final String insertUsersSQL = "INSERT INTO konten(kto, betrag, kunde) VALUES (1, 1000, 'A'), (2, 1000, 'B'), (3, 1000, 'C');";
+	
+	public static void main(String[] args) {
+		// final String url = "jdbc:sqlite:helloFhdw.sqlite.db";
+		final String mysqliteURL = "jdbc:sqlite:helloFhdw.sqlite.db";
+		final String mariadbURL = "jdbc:mariadb://localhost:3306/test?user=transaktionsmanagement&password=Apfelkuchen?!";
+		final String postgreSQLURL = "jdbc:postgresql://localhost/test?user=transaktionsmanagement&password=Apfelkuchen?!";
+			
+			/*
+			 * CREATE TABLE "konten" (
+				"kto"	INTEGER NOT NULL,
+				"betrag"	INTEGER NOT NULL,
+				"kunde"	TEXT NOT NULL,
+				PRIMARY KEY("kto")
+			);
+			 */
+			
+			String[] connections = {mysqliteURL, mariadbURL, postgreSQLURL};
+			for (String url : connections) {
+				
+				try (final Connection conn = DriverManager.getConnection(url)) {
+					System.out.println("Verbunden mit Datenbank " + url);
+					resetDatabase(conn);
+					
+					BankTransferTransaktionThread ab = new BankTransferTransaktionThread(conn, url, "A", "B", BigDecimal.valueOf(100));
+					BankTransferTransaktionThread bc = new BankTransferTransaktionThread(conn, url, "B", "C", BigDecimal.valueOf(100));
+					BankTransferTransaktionThread ca = new BankTransferTransaktionThread(conn, url, "C", "A", BigDecimal.valueOf(100));
+					
+					ab.start();
+					bc.start();
+					ca.start();
+					
+					ab.join();
+					bc.join();
+					ca.join();
+					try(final Connection conn2 = DriverManager.getConnection(url)){
+						listKunden(conn2);
+					}catch (Exception e) {
+						System.out.println("Wir wollten doch nur die Ergebnisse printen.");
+					}
+					
+					
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				System.out.println("****Ende der Datenbankaktionen****\n");
+			}
+			
+			
+//			 listKunden(conn);
+//			//createKunden(conn);
+//			
+//			final String von = "A";
+//			final String nach = "B";
+//			final BigDecimal betrag = BigDecimal.valueOf(100, 0);			
+//			bankTransfer(conn, von, nach, betrag);
+		
+	}
+	
+	
+	/**
+	 * Setzt die Datenbank auf einen Basiszustand zurück.
+	 * @param conn
+	 */
+	private static void resetDatabase(Connection conn) {
+		try (final Statement stmt = conn.createStatement()) {
+			stmt.executeUpdate(deleteKontenSQL);
+			System.out.println("Konten deleted.");
+			stmt.executeUpdate(insertUsersSQL);
+			System.out.println("Users inserted.");
+		} catch (SQLException e) {
+			System.out.println("Database "+ conn +" didn't like being reset.");;
+			e.printStackTrace();
+		}
+	}
+	
+
+	/**
+	 * Ausgabe aller Kunden und des jeweils aktuellen Kontostandes aus der Tabelle `konten`:
+	 * <xmp>
+	 * Alle Kunden
+	 * Kunde A hat 1000 Geldeinheiten
+	 * Kunde B hat 3000 Geldeinheiten
+	 * ENDE Alle Kunden
+	 * </xmp> 
+	 * @param conn Eine aktive JDBC-Datenbankverbindung
+	 */
+	private static void listKunden(final Connection conn) {
+		try (final Statement stmt = conn.createStatement()) {
+			System.out.println("Alle Kunden");
+			try (ResultSet rs = stmt.executeQuery("SELECT kunde, betrag FROM konten")) {
+				while(rs.next()) {
+					final String kunde = rs.getString("kunde");
+					final BigDecimal kontostand = rs.getBigDecimal("betrag");
+					System.out.printf("Kunde %s hat %s Geldeinheiten%n", kunde, kontostand);
+				}
+			}
+			System.out.println("ENDE Alle Kunden");
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+}
